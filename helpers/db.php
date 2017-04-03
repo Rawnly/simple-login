0413c5bc7822d17643cbaa0e4ddee89e38f45cb0
Federico Vitale <fedevitale99@gmail.com>
2017-04-03 15:32:48 +0200

    Update db.php

--- helpers/db.php
+++ helpers/db.php
@@ -1,15 +1,19 @@
 <?php
   error_reporting(E_ERROR | E_PARSE);
-  // DB DATA
+
+  // CREDENTIALS  
+  // Edit with yours.
   $host = '127.0.0.1';
   $name = "test_database";
   $port = 8889;
   $pass = "root";
   $user = "root";
   $server = "$host:$port";
-
+  
+  // Connection init
   $conn = new mysqli($host, $user, $pass, $name, $port);
-
+  
+  // Connection status
   if (!$conn->connect_error) {
     $status = "<span style='color: #28AC34'> Online </span>";
     $class = "online";
